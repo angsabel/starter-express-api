32326c8bd6de105ccf7b30252bb5ba3d09c2a44b
angsabel <142607230+angsabel@users.noreply.github.com>
2023-08-18 23:03:06 +0700

    next 11

--- data.js
+++ data.js
@@ -1,86 +1,83 @@
 const PortfolioData = () => {
-    return [
-      {
-        title: "Caishen: The Cashiers App",
-        description: "Portfolio app to show my skills and experience.",
-        cover: "https://picsum.photos/1920/1080",
-        language: "Typescript",
-        techStack: ["react", "javascript", "chakra-ui"],
-        url: "https://github.com/MA-Ahmad/myPortfolio",
-        live: "https://mahmad.me",
-        description: "Portfolio app to show my skills and experience.",
-        stargazers_count: 0,
-        forks_count: 22,
-      },
-      {
-        title: "My Portfolio",
-        description: "Personal portfolio app to show my skills and experience.",
-        cover: "https://picsum.photos/1920/1081",
-        language: "Typescript",
-        techStack: ["react", "chakraUi"],
-        url: "https://github.com/MA-Ahmad/portfolio",
-        live: "https://ma-ahmad.github.io/portfolio",
-        description: "Personal portfolio app to show my skills and experience.",
-        stargazers_count: 1,
-        forks_count: 14,
-      },
-      {
-        title: "Crea-te: The Web Blog",
-        description:
-          "This portfolio built by using Next.js, ChakraUi and Typescript.",
-        cover: "https://picsum.photos/1920/1078",
-        language: "Typescript",
-        techStack: ["react", "chakra-ui"],
-        url: "https://github.com/MA-Ahmad/dev.to-clone",
-        live: "https://dev-to-clone-ma.vercel.app",
-        description:
-          "This portfolio built by using Next.js, ChakraUi and Typescript.",
-        stargazers_count: 1,
-        forks_count: 5,
-      },
-      {
-        title: "Backend API of Web Blog",
-        description:
-          "This is a notebook app which will help you to create notes for your daily work.",
-        cover: "https://picsum.photos/1920/1076",
-        language: "Javascript",
-        techStack: ["react", "javascript", "chakra-ui"],
-        url: "https://github.com/MA-Ahmad/notebook",
-        live: "https://ma-ahmad.github.io/notebook",
-        description:
-          "This is a notebook app which will help you to create notes for your daily work.",
-        stargazers_count: 0,
-        forks_count: 8,
-      },
-      {
-        title: "Simple Cart App",
-        description:
-          "This is a notebook app which will help you to create notes for your daily work.",
-        cover: "https://picsum.photos/1920/1086",
-        language: "Javascript",
-        techStack: ["chakra-ui", "react", "javascript"],
-        url: "https://github.com/MA-Ahmad/notebook",
-        live: "https://ma-ahmad.github.io/notebook",
-        description:
-          "This is a notebook app which will help you to create notes for your daily work.",
-        stargazers_count: 1,
-        forks_count: 8,
-      },
-      {
-        title: "Uku-Uku App: The Payroll Management",
-        description:
-          "This is a notebook app which will help you to create notes for your daily work.",
-        cover: "https://picsum.photos/1920/1076",
-        language: "Javascript",
-        techStack: ["react", "javascript", "chakra-ui"],
-        url: "https://github.com/MA-Ahmad/notebook",
-        live: "https://ma-ahmad.github.io/notebook",
-        description:
-          "This is a notebook app which will help you to create notes for your daily work.",
-        stargazers_count: 1,
-        forks_count: 1,
-      },
-    ];
-  }
-  
-  module.exports = PortfolioData
+  return [
+    {
+      title: "Caishen: The Cashiers App",
+      description: "A portfolio app showcasing my skills and experience, built using Javascript, React, and Chakra UI.",
+      cover: "https://picsum.photos/1920/1080",
+      language: "Javascript",
+      techStack: ["react", "javascript", "chakra-ui"],
+      url: "https://github.com/MA-Ahmad/myPortfolio",
+      live: "https://mahmad.me",
+      stargazers_count: 0,
+      forks_count: 22,
+    },
+    {
+      title: "My Portfolio",
+      description: "A personal portfolio app highlighting my skills and experience, developed with Javascript, React, and Chakra UI.",
+      cover: "https://picsum.photos/1920/1081",
+      language: "Javascript",
+      techStack: ["react", "chakraUi"],
+      url: "https://github.com/MA-Ahmad/portfolio",
+      live: "https://ma-ahmad.github.io/portfolio",
+      stargazers_count: 1,
+      forks_count: 14,
+    },
+    {
+      title: "Crea-te: The Web Blog",
+      description: "A web blog crafted using Next.js, Chakra UI, and Javascript to showcase my work.",
+      cover: "https://picsum.photos/1920/1078",
+      language: "Javascript",
+      techStack: ["react", "chakra-ui"],
+      url: "https://github.com/MA-Ahmad/dev.to-clone",
+      live: "https://dev-to-clone-ma.vercel.app",
+      stargazers_count: 1,
+      forks_count: 5,
+    },
+    {
+      title: "Backend API of Web Blog",
+      description: "A notebook app aiding in daily work by enabling note creation, built with Javascript, React, and Chakra UI.",
+      cover: "https://picsum.photos/1920/1076",
+      language: "Javascript",
+      techStack: ["react", "javascript", "chakra-ui"],
+      url: "https://github.com/MA-Ahmad/notebook",
+      live: "https://ma-ahmad.github.io/notebook",
+      stargazers_count: 0,
+      forks_count: 8,
+    },
+    {
+      title: "Simple Cart App",
+      description: "A straightforward cart app designed to assist in managing daily tasks, created using Javascript and Chakra UI.",
+      cover: "https://picsum.photos/1920/1086",
+      language: "Javascript",
+      techStack: ["chakra-ui", "react", "javascript"],
+      url: "https://github.com/MA-Ahmad/notebook",
+      live: "https://ma-ahmad.github.io/notebook",
+      stargazers_count: 1,
+      forks_count: 8,
+    },
+    {
+      title: "Uku-Uku App: The Payroll Management",
+      description: "An app for efficient payroll management, built with Javascript, React, and Chakra UI.",
+      cover: "https://picsum.photos/1920/1076",
+      language: "Javascript",
+      techStack: ["react", "javascript", "chakra-ui"],
+      url: "https://github.com/MA-Ahmad/notebook",
+      live: "https://ma-ahmad.github.io/notebook",
+      stargazers_count: 1,
+      forks_count: 1,
+    },
+    {
+      title: "Docs Generator",
+      description: "A tool for generating documents, implemented in Python.",
+      cover: "https://picsum.photos/1920/1076",
+      language: "Python",
+      techStack: ["python"],
+      url: "https://github.com/MA-Ahmad/notebook",
+      live: "https://ma-ahmad.github.io/notebook",
+      stargazers_count: 1,
+      forks_count: 1,
+    },
+  ];
+}
+
+module.exports = PortfolioData;

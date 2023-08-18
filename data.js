57096a7edd80ad3fc2133ea1225369e915c0bcfe
angsabel <142607230+angsabel@users.noreply.github.com>
2023-08-18 22:56:41 +0700

    next 10

--- data.js
+++ data.js
@@ -1,19 +1,19 @@
 const PortfolioData = () => {
     return [
       {
-        title: "Portfolio1",
+        title: "Caishen: The Cashiers App",
         description: "Portfolio app to show my skills and experience.",
         cover: "https://picsum.photos/1920/1080",
         language: "Typescript",
-        techStack: ["Nextjs", "react", "chakraUi"],
+        techStack: ["react", "javascript", "chakra-ui"],
         url: "https://github.com/MA-Ahmad/myPortfolio",
         live: "https://mahmad.me",
         description: "Portfolio app to show my skills and experience.",
-        stargazers_count: 96,
+        stargazers_count: 0,
         forks_count: 22,
       },
       {
-        title: "Portfolio2",
+        title: "My Portfolio",
         description: "Personal portfolio app to show my skills and experience.",
         cover: "https://picsum.photos/1920/1081",
         language: "Typescript",
@@ -21,36 +21,64 @@ const PortfolioData = () => {
         url: "https://github.com/MA-Ahmad/portfolio",
         live: "https://ma-ahmad.github.io/portfolio",
         description: "Personal portfolio app to show my skills and experience.",
-        stargazers_count: 67,
+        stargazers_count: 1,
         forks_count: 14,
       },
       {
-        title: "Portfolio3",
+        title: "Crea-te: The Web Blog",
         description:
           "This portfolio built by using Next.js, ChakraUi and Typescript.",
         cover: "https://picsum.photos/1920/1078",
         language: "Typescript",
-        techStack: ["Nextjs", "chakraUi"],
+        techStack: ["react", "chakra-ui"],
         url: "https://github.com/MA-Ahmad/dev.to-clone",
         live: "https://dev-to-clone-ma.vercel.app",
         description:
           "This portfolio built by using Next.js, ChakraUi and Typescript.",
-        stargazers_count: 54,
+        stargazers_count: 1,
         forks_count: 5,
       },
       {
-        title: "Notebook",
+        title: "Backend API of Web Blog",
         description:
           "This is a notebook app which will help you to create notes for your daily work.",
         cover: "https://picsum.photos/1920/1076",
         language: "Javascript",
-        techStack: ["react", "typescript"],
+        techStack: ["react", "javascript", "chakra-ui"],
         url: "https://github.com/MA-Ahmad/notebook",
         live: "https://ma-ahmad.github.io/notebook",
         description:
           "This is a notebook app which will help you to create notes for your daily work.",
-        stargazers_count: 19,
+        stargazers_count: 0,
         forks_count: 8,
+      },
+      {
+        title: "Simple Cart App",
+        description:
+          "This is a notebook app which will help you to create notes for your daily work.",
+        cover: "https://picsum.photos/1920/1086",
+        language: "Javascript",
+        techStack: ["chakra-ui", "react", "javascript"],
+        url: "https://github.com/MA-Ahmad/notebook",
+        live: "https://ma-ahmad.github.io/notebook",
+        description:
+          "This is a notebook app which will help you to create notes for your daily work.",
+        stargazers_count: 1,
+        forks_count: 8,
+      },
+      {
+        title: "Uku-Uku App: The Payroll Management",
+        description:
+          "This is a notebook app which will help you to create notes for your daily work.",
+        cover: "https://picsum.photos/1920/1076",
+        language: "Javascript",
+        techStack: ["react", "javascript", "chakra-ui"],
+        url: "https://github.com/MA-Ahmad/notebook",
+        live: "https://ma-ahmad.github.io/notebook",
+        description:
+          "This is a notebook app which will help you to create notes for your daily work.",
+        stargazers_count: 1,
+        forks_count: 1,
       },
     ];
   }
